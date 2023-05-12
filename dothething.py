bd4fa58588cf8dcb6f3c5bc7261277ec74dc3db7
Charlie Benger-Stevenson <suityou01@yahoo.co.uk>
2023-05-12 15:57:47 +0100

    Latest

--- dothething.py
+++ dothething.py
@@ -6,14 +6,16 @@ import requests
 import argparse
 
 def main():
-    r = requests.get(args.url)
+    
+    headers = {'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.9; rv:32.0) Gecko/20100101 Firefox/32.0'}
+    r = requests.get(args.url, headers=headers)
     soup = BeautifulSoup(r.content, features="lxml")
 
     title = soup.title.string
     print ('TITLE IS :', title)
 
     meta = soup.find_all('meta')
-
+    print(meta)
     for tag in meta:
         if 'name' in tag.attrs.keys() and tag.attrs['name'].strip().lower() in ['description', 'keywords']:
             # print ('NAME    :',tag.attrs['name'].lower())
